0f8cba3a8c2c2403546311b876d6841c431c71a5
Rishabh <medishame2@gmail.com>
2024-11-26 10:54:30 +0530

    Practice variabls in variables.js

--- javaScript/nodeJS/variables.js
+++ javaScript/nodeJS/variables.js
@@ -1,1 +1,7 @@
-console.log(1)
+switch(a = 1){
+    case 0 : console.log("indore");
+    case 2 : console.log ("pune");
+    // break;
+    case 3 : console.log("dhamnod")
+    default : console.log("nananana")
+}
