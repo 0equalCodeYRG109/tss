7d8f948e3acdafcf9cb24042eb91d3c4dd2cd195
Rishabh <medishame2@gmail.com>
2024-11-27 14:03:59 +0530

    create loop.js js to practice loops

---
+++ javaScript/nodeJS/loops.js
@@ -0,0 +1,18 @@
+let a;
+
+for(a = 1; a <= 10; a++)
+    {
+        console.log(a);
+    }
+
+
+let b = 1;
+
+for(b <= 10; b++;)
+    {
+        console.log(b);
+    }
+
+    /*
+    I am having problems with this code
+    */
